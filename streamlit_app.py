996966c12271cdacbad32d0061802dc3bfb258da
pauloizamora <119440189+pauloizamora@users.noreply.github.com>
2024-10-16 20:29:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '', icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-if fruityvice_response.json():
+fruityvice_json = fruityvice_response.json()
+if fruityvice_json:
     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 else:
     st.write('The was an error trying to retrieve data from Fruityvice: ' + fruityvice_response)
